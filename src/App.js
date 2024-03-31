cf92e4804304b3809c60c60783d7b17ddaf0bd73
Yazid B <islam.yazid.ben@gmail.com>
2024-03-31 03:00:24 +0100

    final commit

--- src/App.js
+++ src/App.js
@@ -9,7 +9,9 @@ import {
   MessengerIcon,
   PlusIcon,
 } from "./icons/imports";
-import { useEffect, useState } from "react";
+import { useRef, useState } from "react";
+import { CSSTransition } from "react-transition-group";
+
 function App() {
   return (
     <Navbar>
@@ -17,7 +19,7 @@ function App() {
       <NavItem icon={<BellIcon />} />
       <NavItem icon={<MessengerIcon />} />
       <NavItem icon={<CaretIcon />}>
-        <p>Home text</p>
+        <DropDownMenu></DropDownMenu>
       </NavItem>
     </Navbar>
   );
@@ -35,12 +37,79 @@ function NavItem(props) {
   const [open, setOpen] = useState(false);
 
   return (
-    <li className="nav-item" onClick={() => setOpen(!open)}>
-      <a href="#" className="icon-button">
+    <li className="nav-item">
+      <a href="#someRef" className="icon-button" onClick={() => setOpen(!open)}>
         {props.icon}
       </a>
       {open && props.children}
     </li>
+  );
+}
+
+function DropDownMenu() {
+  const [activeMenu, setActiveMenu] = useState("main");
+  const [menuHeight, setMenuHeight] = useState(null);
+  const dropdownRef = useRef(null);
+
+  const calcHeight = (el) => {
+    const height = el.offsetHeight;
+    setMenuHeight(height);
+  };
+  function DropdownItem(props) {
+    return (
+      <a
+        href="#someRef"
+        className="menu-item"
+        onClick={() => props.goToMenu && setActiveMenu(props.goToMenu)}
+      >
+        <span className="icon-button">{props.leftIcon}</span>
+        {props.children}
+        <span className="icon-right">{props.rightIcon}</span>
+      </a>
+    );
+  }
+  return (
+    <div className="dropdown" style={{ height: menuHeight }} ref={dropdownRef}>
+      <CSSTransition
+        in={activeMenu === "main"}
+        timeout={500}
+        unmountOnExit
+        classNames="menu-primary"
+        onEnter={calcHeight}
+      >
+        <div className="menu">
+          <DropdownItem>My Profile</DropdownItem>
+          <DropdownItem
+            leftIcon={<CogIcon />}
+            rightIcon={<ChevronIcon />}
+            goToMenu="settings"
+          >
+            Settings
+          </DropdownItem>
+        </div>
+      </CSSTransition>
+      <CSSTransition
+        in={activeMenu === "settings"}
+        timeout={500}
+        classNames="menu-secondary"
+        unmountOnExit
+        onEnter={calcHeight}
+      >
+        <div className="menu">
+          <DropdownItem goToMenu="main" leftIcon={<ArrowIcon />}>
+            <h2>Settings</h2>
+          </DropdownItem>
+          <DropdownItem leftIcon={<BoltIcon />}>Account Settings</DropdownItem>
+          <DropdownItem leftIcon={<BoltIcon />}>
+            Credentials Details
+          </DropdownItem>
+          <DropdownItem leftIcon={<BoltIcon />}>
+            Marketplace Center
+          </DropdownItem>
+          <DropdownItem leftIcon={<BoltIcon />}>Activty & Log</DropdownItem>
+        </div>
+      </CSSTransition>
+    </div>
   );
 }
 
